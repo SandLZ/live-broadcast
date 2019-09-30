9a3b49d01f7a0d5b79b73b001a9446bb929e4bfa
zliu <978949438@qq.com>
2019-09-30 10:41:04 +0800

    change

--- index.d.ts
+++ index.d.ts
@@ -3,19 +3,19 @@ export declare class HMLiveBroadcastOriginal extends IonicNativePlugin {
     startLiveBroadcast(options: HMLiveBroadcastOptions, success?: SuccessCallback<any>, error?: ErrorCallback<any>): Promise<any>;
 }
 export interface HMLiveBroadcastOptions {
-    title: string;
-    needTitle: boolean;
-    autoPlay: boolean;
-    type: number;
-    isViewer: boolean;
-    playUrl: string;
-    pushUrl: string;
-    userName: string;
-    debug: boolean;
-    cameraDefault: number;
-    decodeDefault: boolean;
-    focusDefault: boolean;
-    resolutionDefault: number;
+    title?: string;
+    needTitle?: boolean;
+    autoPlay?: boolean;
+    type?: number;
+    isViewer?: boolean;
+    playUrl?: string;
+    pushUrl?: string;
+    userName?: string;
+    debug?: boolean;
+    cameraDefault?: number;
+    decodeDefault?: boolean;
+    focusDefault?: boolean;
+    resolutionDefault?: number;
 }
 export declare type SuccessCallback<T> = (result?: T) => void;
 export declare type ErrorCallback<T> = (result?: T) => void;
